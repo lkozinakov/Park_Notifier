11f9061b1190cdaf921e2616a80ab3c3bc9ae613
Leon Kozinakov <lkozinakov@yahoo.com>
2019-07-17 02:32:21 -0700

    Working version, checks todays date, checks both types of permits

--- notify.py
+++ notify.py
@@ -1,4 +1,6 @@
 import time
+from dateutil.parser import parse
+from datetime import date as dt
 import sys
 import yagmail
 import getpass
@@ -6,20 +8,20 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
 def Email(newtext, myemail=None, password=None, sendemail=None):
-	if myemail == None:
-		myemail = str(open("email.txt", 'r').read()).partition(':')[0]
-	if password == None:
-		password = str(open("email.txt", 'r').read()).partition(':')[2]
-	if sendemail == None:
-		print("error")
-	yagmail.register(myemail, password)
-	yag = yagmail.SMTP(myemail)
-	yagmail.SMTP(myemail).send(sendemail, newtext)
-	print('sent email')
+    if myemail == None:
+        myemail = str(open("email.txt", 'r').read()).partition(':')[0]
+    if password == None:
+        password = str(open("email.txt", 'r').read()).partition(':')[2]
+    if sendemail == None:
+        print("error")
+    yagmail.register(myemail, password)
+    yag = yagmail.SMTP(myemail)
+    yagmail.SMTP(myemail).send(sendemail, newtext, "You only have a few minutes!\n\nLink: https://www.recreation.gov/permits/233260")
+    print('sent email')
 
-time_to_sleep = 30 #sleep for 10 seconds before trying again
+time_to_sleep = 30 #sleep for 30 seconds before trying again
 number_of_times_checked = 0
-dates_of_interest = ['Tuesday, July 9, 2019', 'Saturday, July 20, 2019', 'Saturday, July 27, 2019', 'Saturday, August 3, 2019', 'Saturday, August 10, 2019', 'Saturday, August 17, 2019', 'Saturday, August 24, 2019', 'Saturday, August 31, 2019']
+dates_of_interest = ['Saturday, July 27, 2019', 'Saturday, August 3, 2019', 'Saturday, August 10, 2019', 'Saturday, August 17, 2019', 'Saturday, August 24, 2019', 'Saturday, August 31, 2019']
 
 mailing_list = [line.rstrip('\n') for line in open('mailing_list.txt')]
 
@@ -27,21 +29,24 @@ driver = webdriver.Chrome()
 driver.get("https://www.recreation.gov/permits/233260")
 
 while(1):
-	for date in dates_of_interest:
-		time.sleep(2) # Allow some time to fetch results
-		driver.find_element_by_id('division-selection-select').click()
-		driver.find_element_by_class_name('rec-select-options').click() #select the first item in drop-down which is the day-hike option
-		time.sleep(2) # Allow some time to fetch results
-		driver.find_element_by_id('number-input').send_keys("1") #select number of people, 1 is minimum
-		#fetch the availability for the specified date
-		availability = driver.find_element_by_xpath("//button[@aria-label='" + date + "']/div[1]/div[1]").get_attribute('aria-label')
+    for type in '12':
+        for date in dates_of_interest:
+            if(parse(str(dt.today())) <= parse(date)):
+                time.sleep(4) # Allow some time to fetch results
+                driver.find_element_by_id('division-selection-select').click()
+                #select the item in drop-down for type of permit (day vs overnight)
+                driver.find_element_by_xpath("//div[@id='division-selection']/div/div/ul/li[" + type + "]").click()
+                time.sleep(4) # Allow some time to fetch results
+                driver.find_element_by_id('number-input').send_keys("1") #select number of people, 1 is minimum
+                #fetch the availability for the specified date
+                availability = driver.find_element_by_xpath("//button[@aria-label='" + date + "']/div[1]/div[1]").get_attribute('aria-label')
 
-		if("Available" in availability):
-			output_string = "Found " + availability.split()[2] + " slots available for " + date
-			for email in mailing_list:
-				Email(output_string, sendemail=email)
-		else:
-			print("Sorry, nothing available")
+                if("Available" in availability):
+                    output_string = "Found " + availability.split()[2] + " slots available for " + date
+                    for email in mailing_list:
+                        Email(output_string, sendemail=email)
+                else:
+                    print("Sorry, nothing available")
 
-		driver.refresh()
-	time.sleep(time_to_sleep)
+                driver.refresh()
+    time.sleep(time_to_sleep)
